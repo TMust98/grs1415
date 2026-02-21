7c43d35fed819f4847a19f8ac3932158f5afdb13
TMust98 <timmust98@gmail.com>
2026-02-21 18:58:16 +0300

    Первый Flask

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'John'}
+    return render_template('index.html', title="Home", user=user)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
