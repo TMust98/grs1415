c7d45c09adff0c214c9892e083da3631a2501591
TMust98 <timmust98@gmail.com>
2026-03-14 10:33:43 +0300

    flask2

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'John'}
-    return render_template('index.html', title="Home", user=user)
+    return render_template('index.html', title="Home")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Добро пожаловать, {form.username.data}!')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Вход', form=form)
