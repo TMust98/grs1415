04e9fedcff86365d8f261e8427f08e5078fed117
TMust98 <timmust98@gmail.com>
2026-03-28 11:45:45 +0300

    flask 4

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
+from xml.dom import ValidationErr
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +11,16 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрировтаься')
+
+
+    def validate_username(self, username):
+        username = User.query.filter_by(username=username.data).first()
+        if username is not None:
+            raise ValidationError('Это имя пользователя уже занято')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
-
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -11,8 +14,48 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Добро пожаловать, {form.username.data}!')
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Неправильный логи или пароль')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash("Успешный вход в систему")
+        next_page = request.args.get('next')
+        if not next_page:
+            next_page = url_for('index')
+        return redirect(next_page)
+
     return render_template('login.html', title='Вход', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("Вы вышли из системы")
+    return redirect(url_for('index'))
+
+
+@app.route('/lk', methods=['GET', 'POST'])
+@login_required
+def lk():
+    text = f"Это личный кабинет пользователя {current_user.username}"
+    return render_template('lk.html', text=text)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data)
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Успешная регистрация')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
